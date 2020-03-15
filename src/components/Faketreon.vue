731c3be6379e625202fcd5f7d74de7c4aab42005
evansoohoo <evan.soohoo@gmail.com>
2020-03-15 11:33:13 -0700

    added little circle photo

--- src/components/Faketreon.vue
+++ src/components/Faketreon.vue
@@ -30,6 +30,29 @@
       <v-btn text small>Create on Patreon</v-btn>
       <v-btn text small>Log in</v-btn>
     </v-app-bar>
+
+    <v-container fluid>
+    <v-row justify="space-around">
+      <v-col cols="12">
+        
+        <v-img src="https://i.picsum.photos/id/0/5616/3744.jpg" aspect-ratio="2.9"></v-img>
+        <v-card flat tile class="d-flex">
+          <v-row justify="space-around">
+              
+        </v-row>
+        
+        </v-card>
+      </v-col>
+      </v-row>
+      </v-container>
+      <v-row justify="space-around">
+        <v-col cols="12">
+        <v-avatar color="indigo" size="100">
+          <span class="white--text headline"><v-img src="https://i.picsum.photos/id/166/200/300.jpg" position='center'></v-img></span> 
+          <!--<span class="white--text headline">36</span>-->
+        </v-avatar>
+        </v-col>
+        </v-row>
   </div>
 </template>
 
